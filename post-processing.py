a3e3fd4a8358e38f7df7988f92bef5fd5342abe9
Jonathan Liu <liujonathan24@gmail.com>
2022-04-04 22:56:59 -0400

    final vectors

--- post-processing.py
+++ post-processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 LBP = pd.read_json("./LBP_vector_dataframe.json")
 ORB = pd.read_json("./ORB_vector_dataframe.json")
@@ -18,11 +19,27 @@ for a in range(920): # Number of photos
         print('whut????')
 
 
-print(len(overall_list[0][1]))
+#print(len(overall_list[0][1]))
 
 # C is a small constant
 c = 1e-5
 
-for a in range(920):
-    abcd = overall_list[a][1]
+for b in range(920):
+    abcd = overall_list[b][1] # 91 length: 59 + 32
+    avg = 0
+    r = 0
+    for a in abcd:
+        r += (a-avg)**2
+        avg += a/91
+    #print(avg)
+        
+    new = []
+    for a in abcd:
+        new.append(100 * (a-avg)/(r+c))
+    overall_list[b][1] = new
+
+vector_dataframe = pd.DataFrame(data=np.array(overall_list), columns=['Emotion', 'Final_Vector'])
+vector_dataframe.to_json("./Final_Vectors.json", orient='columns')
+
+
 
